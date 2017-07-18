b27d26a4ed96b16a3a6eb2d90781b9584b322d0b
jrojasgon <johannarojasgon@gmail.com>
2017-07-18 17:17:23 +0200

    config webpack

--- src/app/contacts/contact.module.js
+++ src/app/contacts/contact.module.js
@@ -1,4 +1,4 @@
 // avec export, on peut importer ailleurs le contactModule
-export const contactModule = function () {
+export const contactModule = function () {//grace au webpack on va remplacer le const par var pour être compatible avec d'autres versions de browser qui ne supportent pas encore le ECS6
   console.log('contactModule');
 }
--- webpack.config.js
+++ webpack.config.js
@@ -6,8 +6,26 @@ module.exports = {
     filename: "bundle.[hash].js", // resultat d'un build (il va regrouper tous les js), le hash facilite les pb de cache car il genere à chaque fois un nouveau nom de fichier
   },
   plugins: [
-    new HtmlWebpackPlugin( {
+    new HtmlWebpackPlugin({
       template: './src/index.html'
     }),
-  ]
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [['env', {
+              target: {
+                browsers: ['IE 11', 'Firefox ESR', 'last 2 versions']
+              }
+            }]]
+          }
+        }
+      }
+    ]
+  }
 };
